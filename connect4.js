7ce177db3cb246cd0b164be6eae51a8a58e2e21e
Rachel Or <rachelor7@gmail.com>
2022-07-04 20:57:50 -0400

    find winners

--- connect4.js
+++ connect4.js
@@ -4,6 +4,8 @@ var currPlayer = playerRed;
 
 var gameOver = false;
 var board;
+// keep track of how many pucks on bottom
+var currColumns;
 
 var rows = 6;
 var columns = 7;
@@ -14,6 +16,7 @@ window.onload = function () {
 
 const setGame = () => {
   board = [];
+  currColumns = Array.from({ length: 7 }).fill(5);
 
   for (let r = 0; r < rows; r++) {
     let row = [];
@@ -37,13 +40,17 @@ const setPiece = (e) => {
   if (gameOver) return;
 
   let coords = e.target.id.split("-");
-  let r = parseInt(coords[0]);
   let c = parseInt(coords[1]);
+  let r = currColumns[c];
+
+  // column is filled
+  if (r < 0) return;
 
   board[r][c] = currPlayer;
+  currColumns[c] -= 1;
 
   // current tile clicked
-  tile = e.target;
+  tile = document.getElementById(r.toString() + "-" + c.toString());
 
   // change color in that tile
   if (currPlayer === playerRed) {
@@ -53,4 +60,82 @@ const setPiece = (e) => {
     tile.classList.add("yellow-piece");
     currPlayer = playerRed;
   }
+
+  checkWinner();
+};
+
+const checkWinner = () => {
+  // check horizontally (sliding window)
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] !== "") {
+        if (
+          board[r][c] === board[r][c + 1] &&
+          board[r][c + 1] === board[r][c + 2] &&
+          board[r][c + 2] === board[r][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // check vertically
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 3; r++) {
+      if (board[r][c] !== "") {
+        if (
+          board[r][c] === board[r + 1][c] &&
+          board[r + 1][c] === board[r + 2][c] &&
+          board[r + 2][c] === board[r + 3][c]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // check anti-diagonally
+  for (let r = 0; r < rows - 3; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] !== "") {
+        if (
+          board[r][c] === board[r + 1][c + 1] &&
+          board[r + 1][c + 1] === board[r + 2][c + 2] &&
+          board[r + 2][c + 2] === board[r + 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // check diagonally
+  for (let r = 3; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] !== "") {
+        if (
+          board[r][c] === board[r - 1][c + 1] &&
+          board[r - 1][c + 1] === board[r - 2][c + 2] &&
+          board[r - 2][c + 2] === board[r - 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+};
+
+const setWinner = (r, c) => {
+  let winner = document.getElementById("winner");
+  if (board[r][c] === playerRed) {
+    winner.innerText = "Red Wins";
+  } else {
+    winner.innerText = "Yellow Wins";
+  }
+  gameOver = true;
 };

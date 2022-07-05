d6a317ff17c8124864fb55691f1dfec553bcde6f
Rachel Or <rachelor7@gmail.com>
2022-07-04 20:12:33 -0400

    set up board

---
+++ connect4.js
@@ -0,0 +1,46 @@
+var playerRed = "R";
+var playerYellow = "Y";
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+
+window.onload = function () {
+  setGame();
+};
+
+const setGame = () => {
+  board = [];
+
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      // JS
+      row.push("");
+
+      // HTML
+      // <div id="0-0" class="tile"></div>
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      tile.addEventListener("click", setPiece);
+      document.getElementById("board").append(tile);
+    }
+    board.push(row);
+  }
+};
+
+const setPiece = (e) => {
+  if (gameOver) return;
+
+  let coords = e.target.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  board[r][c] = currPlayer;
+
+  tile = e.target;
+};
